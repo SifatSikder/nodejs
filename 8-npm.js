9fb07d966f99fe97779b0273023b6f0b722d3f02
SifatSikder <bsse1221@iit.du.ac.bd>
2023-01-20 22:59:35 +0600

    updated

--- 8-npm.js
+++ 8-npm.js
@@ -11,4 +11,14 @@
 //npm init -y (everything default)
 
 const _ = require('lodash');
-console.log(_.flattenDeep([1,[2,[3,[4,5]]]]));
+console.log(_.flattenDeep([1,[2,[3,[4,6]]]]));
+
+//installing nodemon
+//npm i nodemon -D here -D means it will setup the dependency as devdependencies
+//after that we will go to the package.json file and in the script we will write
+//"start": "node \"/home/sifat/3-1/3-1/Web Technology/NodeJS/8-npm.js\"", ==> to execute this in terminal we will write npm start or npm run start  
+//"dev": "nodemon \"/home/sifat/3-1/3-1/Web Technology/NodeJS/8-npm.js\"" ==>to execute this in terminal we will write npm run dev
+//when we make a change in our code nodemon will change it in the terminal. So we dont have to run the code everytime
+
+//uninstall a package
+//node uninstall packagename
